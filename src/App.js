4636a5b925ec5e0544da83e39de57f894be56c28
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-05 18:23:10 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CenteredCard from "./Components/CenteredCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CenteredCard><h2>Children 1</h2></CenteredCard>
+      <CenteredCard><h2>Children 2</h2></CenteredCard>
+      <CenteredCard><h2>Children 3</h2></CenteredCard>
     </div>
   );
 }
---
+++ src/Components/CenteredCard/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css"
+function CenteredCard({children}){
+
+    return(
+        <div className="CenteredCard">
+            <span>
+                {children}
+            </span>
+        </div>
+    )
+}
+
+export default CenteredCard;
